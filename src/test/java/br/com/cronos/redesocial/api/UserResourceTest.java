98ccaca13b592a699d05991a088b141c7411fb84
Rodrigo Souza <roferreira122@gmail.com>
2023-04-01 08:49:02 -0300

    implementando teste de cenario de erro ao criar usuario

--- src/test/java/br/com/cronos/redesocial/api/UserResourceTest.java
+++ src/test/java/br/com/cronos/redesocial/api/UserResourceTest.java
@@ -1,15 +1,19 @@
 package br.com.cronos.redesocial.api;
 
 import br.com.cronos.redesocial.api.dto.CreateUserRequest;
+import br.com.cronos.redesocial.api.dto.ResponseError;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import org.h2.command.ddl.CreateUser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
 class UserResourceTest {
@@ -47,6 +51,27 @@ class UserResourceTest {
 
         assertEquals(201, response.getStatusCode());
         assertNotNull(response.jsonPath().getString("id"));
+    }
+
+    @Test
+    @DisplayName("should return error when json is not valid")
+    void createUserErrorTest() {
+        var user = new CreateUserRequest();
+        user.setName(null);
+        user.setAge(null);
+
+        var response = given()
+                .contentType(ContentType.JSON)
+                .body(user)
+            .when()
+                .post("/api/v1/users")
+            .then()
+                .extract().response();
+
+        assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.getStatusCode());
+        List<Map<String, String>> errors = response.jsonPath().getList("errors");
+        assertNotNull(errors.get(0).get("message"));
+        assertNotNull(errors.get(1).get("message"));
     }
 
     @Test
